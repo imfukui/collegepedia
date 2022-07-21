42cae61d0422a37f18531ebbb13db4a448021d29
Mieka Fukui <fukum@MiekanoMacBook-Pro.local>
2022-07-21 15:37:05 +0900

    college controller作成に伴いルーティング変更する

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/', to: 'colleges#home'
+  resources :colleges, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
