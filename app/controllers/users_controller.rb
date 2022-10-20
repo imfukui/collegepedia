6826f962e3e2d189f614ea8d36d4e04f0c9d6eac
Mieka Fukui <fukum@MiekanoMacBook-Pro.local>
2022-10-20 16:20:57 +0900

    コード整形のためpartial化する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @collreviews = @user.collreviews
+    @course_reviews = @user.course_reviews
   end
 end
